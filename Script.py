af80141fbcee909167cad7518eba16e0d428e217
isbahulhaq <167408032+isbahulhaq@users.noreply.github.com>
2026-03-27 21:47:45 +0530

    Add Playwright dependency installation commands

--- Script.py
+++ Script.py
@@ -1,1 +1,9 @@
-Nothing
+!playwright install-deps
+!sudo apt-get install -y libnss3 \
+                     libnspr4 \
+                     libatk-bridge2.0-0 \
+                     libdrm2 \
+                     libxkbcommon0 \
+                     libatspi2.0-0 \
+                     libgbm1 \
+                     libasound2
